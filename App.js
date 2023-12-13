5988a268c2ab7fc7c8cba0893b181f4bab9b7f50
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2023-12-13 10:48:32 -0800

    Set the Safe Area View and added Image

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Let's start at the very beginning!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Image
+        source={{
+          width: 200,
+          height: 300,
+          uri: 'https://picsum.photos/200/300',
+        }}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +20,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
