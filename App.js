43487c7badd80c577a2b92a74ff4ceea9e5b9980
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2023-12-14 15:30:47 -0800

    Flexbox

--- App.js
+++ App.js
@@ -1,37 +1,26 @@
-import {
-  StyleSheet,
-  SafeAreaView,
-  StatusBar,
-  Platform,
-  View,
-} from 'react-native';
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from '@react-native-community/hooks';
+import React from 'react';
+import { View } from 'react-native';
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: 'dodgerblue',
-          width: '100%',
-          height: landscape ? '100%' : '30%',
-        }}
-      ></View>
-    </SafeAreaView>
+    <View style={{
+      backgroundColor: "#fff",
+      flex: 1,
+    }}>
+      <View style={{
+        backgroundColor: "dodgerblue",
+        flex: 1,
+      }}/>
+      <View style={{
+        backgroundColor: "gold",
+        flex: 1,
+      }}/>
+      <View style={{
+        backgroundColor: "tomato",
+        flex: 1,
+      }}/>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'orange',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+
