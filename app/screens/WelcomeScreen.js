8fbf612e341e06b93df79588bbcd0d32f4e91b54
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2023-12-14 18:13:27 -0800

    Building the welcome screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,18 +1,46 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground
       style={styles.background}
-      source={require('../assets/')}
-    ></ImageBackground>
+      source={require('../assets/background.jpg')}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require('../assets/logo.png')} />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  loginButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#4ECDC4',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 70,
+    alignItems: 'center',
+  },
+  registerButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#fc5c65',
   },
 });
 
