3bcb4dbeb6101a80bf6a24bdc53ff52c2376c9ef
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2023-12-14 16:45:28 -0800

    Finished learning flexbox

--- App.js
+++ App.js
@@ -3,24 +3,39 @@ import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-      backgroundColor: "#fff",
-      flex: 1,
-    }}>
-      <View style={{
-        backgroundColor: "dodgerblue",
+    <View
+      style={{
+        backgroundColor: '#fff',
         flex: 1,
-      }}/>
-      <View style={{
-        backgroundColor: "gold",
-        flex: 1,
-      }}/>
-      <View style={{
-        backgroundColor: "tomato",
-        flex: 1,
-      }}/>
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: 'gold',
+          width: 100,
+          height: 100,
+          top: 40,
+          left: 30,
+          position: "absolute"
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: 'tomato',
+          width: 100,
+          height: 100,
+        }}
+      />
     </View>
   );
 }
-
-
