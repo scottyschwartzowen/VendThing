311b28cbe08d39cda3f3a26b9d61c76f4ca98881
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2023-12-18 13:13:53 -0800

    Creating card component

--- App.js
+++ App.js
@@ -6,9 +6,22 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import AppText from './app/components/AppText';
 import AppButton from './app/components/AppButton';
+import Card from './app/components/Card';
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <View
+      style={{
+        backgroundColor: '#f8f4f4',
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card
+        title="Red jacket for sale!"
+        subTitle="$100"
+        image={require('./app/assets/jacket.jpg')}
+      />
+    </View>
   );
 }
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Text, StyleSheet, Platform } from 'react-native';
 
-function AppText({ children }) {
-  return <Text style={styles.text}>{children}</Text>;
+import colors from '../config/colors';
+
+function AppText({ children, style }) {
+  return <Text style={[styles.text, style]}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
   text: {
-    color: 'teal',
+    color: colors.black,
     fontSize: 20,
     fontWeight: 'bold',
     fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
