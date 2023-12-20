4eea3a6eba8c9a82eef1e37017331d203ae9cd4e
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2023-12-19 17:30:59 -0800

    Handling swipes, deletes and refresh

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,10 +1,12 @@
+import React, { useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 
 import ListItem from '../components/ListItem';
 import Screen from '../components/Screen';
 import ListItemSeparator from '../components/ListItemSeparator';
+import ListItemDeleteAction from '../components/ListItemDeleteAction';
 
-const messages = [
+const initialMessages = [
   {
     id: 1,
     title: 'T1',
@@ -20,6 +22,13 @@ const messages = [
 ];
 
 function MessagesScreen(props) {
+  const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleDelete = (message) => {
+    setMessages(messages.filter((m) => m.id !== message.id));
+  };
+
   return (
     <Screen>
       <FlatList
@@ -31,9 +40,23 @@ function MessagesScreen(props) {
             subTitle={item.description}
             image={item.image}
             onPress={() => console.log('Message selected', item)}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
           />
         )}
         ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 2,
+              title: 'T2',
+              description: 'D2',
+              image: require('../assets/scotty.jpg'),
+            },
+          ]);
+        }}
       />
     </Screen>
   );
