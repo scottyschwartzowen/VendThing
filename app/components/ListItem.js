13887de26f16c52b147edf8c5aec19b0a857e3f9
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2023-12-20 14:56:09 -0800

    Extending the list item component

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -12,16 +12,26 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import AppText from './AppText';
 import colors from '../config/colors';
 
-function ListItem({ title, subTitle, image, onPress, renderRightActions }) {
+function ListItem({
+  title,
+  subTitle,
+  image,
+  ImageComponent,
+  onPress,
+  renderRightActions,
+}) {
   return (
     <GestureHandlerRootView>
       <Swipeable renderRightActions={renderRightActions}>
         <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
           <View style={styles.container}>
-            <Image style={styles.image} source={image} />
-            <View>
+            {ImageComponent}
+            {image && <Image style={styles.image} source={image} />}
+            <View style={styles.detailsContainer}>
               <AppText style={styles.title}>{title}</AppText>
-              <AppText style={styles.subTitle}>{subTitle}</AppText>
+              {subTitle && (
+                <AppText style={styles.subTitle}>{subTitle}</AppText>
+              )}
             </View>
           </View>
         </TouchableHighlight>
@@ -35,11 +45,14 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     padding: 15,
   },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: 'center',
+  },
   image: {
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    marginRight: 10,
+    width: 70,
+    height: 70,
+    borderRadius: 35,
   },
   subTitle: {
     color: colors.medium,
