ae2794b3cc97c4bbfa9c585f79eede8dd3faf33b
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2023-12-20 14:30:18 -0800

    Built a reusable icon component

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import { View } from 'react-native';
+
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import AppText from './app/components/AppText';
@@ -9,7 +10,13 @@ import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" size={100} />
+    </Screen>
+  );
 }
