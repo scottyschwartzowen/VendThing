86768ee03e1c06717f0399620afc05200dfaf326
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2023-12-21 10:29:33 -0800

    Extracting default styles into config file

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { Text, StyleSheet, Platform } from 'react-native';
+import { Text } from 'react-native';
 
 import colors from '../config/colors';
+import defaultStyles from '../config/styles';
 
 function AppText({ children, style }) {
-  return <Text style={[styles.text, style]}>{children}</Text>;
+  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
 }
-
-const styles = StyleSheet.create({
-  text: {
-    color: colors.black,
-    fontSize: 20,
-    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
-  },
-});
 
 export default AppText;
