3d8784218be25588b78253db88a65842ffa34046
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2023-12-22 09:48:39 -0800

    Setup Formik in login screen to replace useState

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import LoginScreen from './app/screens/LoginScreen';
 
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import AccountScreen from './app/screens/AccountScreen';
+import ListingsScreen from './app/screens/ListingsScreen';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+
 export default function App() {
   return <LoginScreen />;
 }
