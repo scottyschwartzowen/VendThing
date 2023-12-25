fde75d4a9de08beb7c7175778318d10824aa123c
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2023-12-25 10:52:13 -0800

    Refactor- updated media library access due to deprecation warning using imageUri

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
-import React, { useState } from 'react';
-import LoginScreen from './app/screens/LoginScreen';
+import React, { useEffect, useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import { Image, Button } from 'react-native';
 
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import RegisterScreen from './app/screens/RegisterScreen';
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import Screen from './app/components/Screen';
 
 export default function App() {
-  return <ListingsScreen />;
+  const [imageUri, setImageUri] = useState();
+  const [assets, setAssets] = useState([]);
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted) alert('You need to enable permission to access the library.');
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setImageUri(result.assets[0].uri);
+        setAssets(result.assets);
+      }
+    } catch (error) {
+      console.error('Error selecting image:', error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
