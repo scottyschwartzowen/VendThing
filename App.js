9be474ff309354eb8e839efe8b37ceab765696f4
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2023-12-26 09:05:21 -0800

    Refactor app component using results.assets[0].uri

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
 
 import Screen from './app/components/Screen';
+import { Button, Image } from 'react-native';
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
   const requestPermission = async () => {
     const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (!granted) alert('You need to enable permission to access the library');
@@ -13,5 +16,20 @@ export default function App() {
     requestPermission();
   }, []);
 
-  return <Screen></Screen>;
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled) setImageUri(result.assets[0].uri);
+      // using "result.assets[0].uri" because "result.uri" is deprecated after SDK 48
+    } catch (error) {
+      console.log('Error reading an image', error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ height: 200, width: 200 }} />
+    </Screen>
+  );
 }
